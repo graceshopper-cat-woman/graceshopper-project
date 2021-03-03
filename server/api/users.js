77612aad842c60588d4274c849273fc60cad591a
Hatibe Kabulantok <hatibekara@gmail.com>
2021-03-03 11:40:04 -0800

    BE: Creates utils folder with middleware

--- server/api/users.js
+++ server/api/users.js
@@ -1,21 +1,24 @@
 const router = require('express').Router()
-const {User, Order, Cart} = require('../db/models')
+const {User, Order, MugOrder} = require('../db/models')
+const adminsOnly = require('../utils/adminsOnly')
+const usersOnly = require('../utils/usersOnly')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', adminsOnly, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email', 'admin']
+      attributes: ['id', 'email', 'isAdmin']
     })
     //only send data if the user is authorized to view the content
-    if (req.body.admin) {
-      res.json(users)
-    } else {
-      res.status(401).send('Role not authorized to view this data')
-    }
+    res.json(users)
+    // if (req.body.isAdmin) {
+    // } else {
+    //   res.status(401).send('Role not authorized to view this data')
+    // }
+    console.log('REQ.USER', req.user)
   } catch (err) {
     next(err)
   }
@@ -25,9 +28,13 @@ router.get('/', async (req, res, next) => {
 router.get('/:userId', async (req, res, next) => {
   try {
     const user = await User.findOne({
-      where: {userId: req.params.id},
-      include: Order,
-      Cart
+      where: {id: req.params.userId},
+      include: {
+        model: Order,
+        where: {
+          userId: req.params.userId
+        }
+      }
     })
 
     res.send(user)
