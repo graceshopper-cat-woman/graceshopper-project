364fe0a29395fe6337e4b6c9fc98d6f8e1b3ba27
Ari Kamarchevakul <snowbearypie@gmail.com>
2021-03-05 16:05:47 -0600

    implemented single Mug add cart button

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
+
 //action type
 const GET_CART = 'GET_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
 
 const getCart = cart => ({
   type: GET_CART,
+  cart
+})
+
+const _addToCart = cart => ({
+  type: ADD_TO_CART,
   cart
 })
 
@@ -19,6 +26,19 @@ export const fetchCart = () => {
   }
 }
 
+export const addToCart = (id, quantity) => {
+  return async dispatch => {
+    try {
+      console.log(id, 'id', quantity, 'quantity')
+      const {data} = await axios.post('/api/carts', {id, quantity})
+      console.log(data)
+      dispatch(_addToCart(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
 //INITIAL STATE
 let initialState = []
 
@@ -27,6 +47,8 @@ export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case GET_CART:
       return action.cart
+    case ADD_TO_CART:
+      return [...state, action.cart]
     default:
       return state
   }
--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'))
 
 router.use('/mugs', require('./mugs'))
 
+router.use('/carts', require('./carts'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
