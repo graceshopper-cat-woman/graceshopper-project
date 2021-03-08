549a5d57302f9985aaec8a28fa98a55716765eac
Traci <tas1013@gmail.com>
2021-03-08 00:15:16 -0600

    FE: Styled landing page and navbar for logged in user

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
-export function Landing() {
+export function Landing({firstName}) {
+  if (firstName) {
+    console.log('firstName --> ', firstName)
+    return (
+      <>
+        <div>
+          <div className="hero-image">
+            <div className="hero-text">
+              <span id="hero-h">Cozy Up, {firstName} </span>
+              <span id="hero-p"> Find your perfect mug this season</span>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
   return (
     <>
       <div>
